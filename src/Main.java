71240ede12cdad950a0722f7bac89b7d61fe9e0d
Jay <samshrestha1@hotmail.com>
2023-05-25 22:32:28 -0400

    first commit directory

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Animal jay = new Animal("Jay", 10);
+        jay.setName("Nischu");
+        jay.setAge(100);
+        List<String> text = jay.getText();
+        List<Character> collect = text.stream().map(val -> val.charAt(1)).toList();
+        text.forEach(val->System.out.print("Name: "+collect + "\n"));
+    }
+}
