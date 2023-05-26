43bb8976d7315bc96a4f500fae9b91b5f614bdcf
Jay <samshrestha1@hotmail.com>
2023-05-25 22:20:43 -0400

    first commit

---
+++ src/Animal.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Animal {
+    private String name;
+    private int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getText() {
+        List<String> list = new ArrayList<>();
+
+        for (int i = 1; i <= this.age; i++) {
+            list.add(this.name + i);
+//            System.out.println("name = " + this.name + i);
+        }
+        return list;
+    }
+}
